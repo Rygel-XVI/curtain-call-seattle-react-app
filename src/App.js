5a4c0d3fcb1a8a90592411b1b848c90f087144d5
Rygel-XVI <micha232@umn.edu>
2019-06-06 13:45:54 -0700

    added fetch to api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './containers/Nav'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import TheaterList from './containers/TheaterList'
 import DateSearch from './containers/DateSearch'
+import UpdateButtonContainer from './containers/UpdateButtonContainer'
 
-function App() {
-  return (
-    <div className="App">
-    <Router>
-    <React.Fragment>
-    <Switch>
-    <Route exact path="/home" component={App}/>
-    <Route exact path="/theater" component={TheaterList}/>
-    <Route exact path="/date" component={DateSearch}/>
-    </Switch>
-    </React.Fragment>
-    <Nav />
-    <p>App Component!</p>
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      theaters: []
+    }
+  }
 
-    </Router>
-    </div>
-  );
+  getTheaters() {
+    fetch("http://localhost:3000/theater.json")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        theaters: data
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getTheaters()
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <Router>
+      <React.Fragment>
+      <Switch>
+      <Route exact path="/home" component={App}/>
+      <Route exact path="/theater" component={TheaterList}/>
+      <Route exact path="/date" component={DateSearch}/>
+      </Switch>
+      </React.Fragment>
+      <Nav />
+      <p>App Component!</p>
+      <p>add update/rescrape website button</p>
+      <UpdateButtonContainer />
+      </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/containers/UpdateButtonContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UpdateButtonContainer extends Component {
+
+  render() {
+    return (
+      <div>
+        ButtonContainer
+      </div>
+    )
+  }
+}
+
+export default UpdateButtonContainer;
